cff020d1352aadd08f716ddbc4b0c08b0e1fcc51
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-04-07 16:01:33 -0400

    Revamped new commit: Removed all existing files and folders and build new project. Created src folder and main files for html css and javascript

--- modules/methods/renderCopyRightYear.js
+++
@@ -1,5 +0,0 @@
-export function renderCopyRightYear(containerId) {
-    let copyRightYearContainer = document.getElementById(containerId)
-    let year = new Date().getFullYear()
-    copyRightYearContainer.innerHTML = year
-}
